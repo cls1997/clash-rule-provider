bb8a21b165c7885247d5d02fc1da44ef1bb16986
Lingshu Chen <chenls1997@gmail.com>
2020-08-09 19:25:05 +0800

    update: refactor && replace YAML implementation

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
 import base64
+import copy
 import re
 from enum import Enum
-
-import yaml
+from typing import Tuple
 
 DEBUG = True
 
@@ -22,10 +24,108 @@ class LineType(Enum):
     OTHER = "Not matches any line type."
 
 
-def fetch_gfwlist():
-    with open("../gfwlist/gfwlist.txt", "rb") as _gfwlist:
-        _content = _gfwlist.read()
-        return base64.decodebytes(_content).decode('utf-8')
+class Context:
+    def __init__(self):
+        self._domain = set()
+        self._ip = set()
+
+    def ip(self) -> set:
+        return copy.deepcopy(self._ip)
+
+    def domain(self) -> set:
+        return copy.deepcopy(self._domain)
+
+    def add(self, value: str, type: LineType):
+        if type is LineType.IP:
+            self._ip.add(value)
+        elif type is LineType.DOMAIN:
+            self._domain.add(value)
+        else:
+            pass
+
+
+class Input(object):
+    def input(self, context: Context) -> Input:
+        return self
+
+
+class Output(object):
+    def output(self, context: Context) -> None:
+        pass
+
+
+class FileOutput(Output):
+    def __init__(self, filename: str):
+        self._filename = filename
+
+    def output(self, context: Context) -> None:
+        pass
+
+
+class ClashProviderRuleYml(FileOutput):
+    def __init__(self, filename: str):
+        super().__init__(filename)
+
+    def output(self, context: Context) -> None:
+        import ruamel.yaml
+        yaml = ruamel.yaml.YAML()
+        yaml.indent(sequence=4, offset=4)
+        domain_set, ip_set = context.domain(), context.ip()
+        out = dict(payload=["DOMAIN-SUFFIX," + x for x in domain_set])
+        with open(self._filename, 'w') as file:
+            yaml.dump(out, file)
+
+
+class DnsmasqIpsetRuleOutput(FileOutput):
+    def __init__(self, filename: str, ipset_name: str, dns: Tuple[str, int]):
+        super().__init__(filename)
+        self._ipset_name = ipset_name
+        self._dns = dns
+
+    def output(self, context: Context) -> None:
+        import datetime
+        with open(self._filename, "w") as f:
+            f.write('# gfw list ipset rules for dnsmasq\n')
+            f.write('# updated on ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
+            f.write('#\n')
+            for domain in context.domain():
+                f.write('server=/.%s/%s#%s\n' % (domain, self._dns[0], self._dns[1]))
+                f.write('ipset=/.%s/%s\n' % (domain, self._ipset_name))
+
+
+class GFWListInput(Input):
+    def __init__(self, filename, encoding="utf-8"):
+        self._filename = filename
+        self._encoding = encoding
+
+    @staticmethod
+    def parse_line(line: str) -> (str, LineType):
+        result = None
+        line_type = LineType.OTHER
+        if comment_re.match(line):
+            line_type = LineType.COMMENT
+        elif domain_re.match(line):
+            result = domain_re.findall(line)[-1]
+            if ip_re.match(line):
+                line_type = LineType.IP
+            else:
+                line_type = LineType.DOMAIN
+        else:
+            pass
+        log_line(line, line_type)
+        return result, line_type
+
+    def input(self, context: Context) -> Context:
+        _result = None
+        with open(self._filename, "rb") as f:
+            _content = f.read()
+            _result = base64.decodebytes(_content).decode(self._encoding)
+        if _result:
+            _result = _result.splitlines()
+            for line in _result:
+                result, line_type = self.parse_line(line)
+                context.add(result, line_type)
+        return context
 
 
 def log_line(line: str, line_type: LineType):
@@ -33,42 +133,15 @@ def log_line(line: str, line_type: LineType):
         print(line_type.value, line)
 
 
-def parse_line(line: str):
-    result = None
-    line_type = LineType.OTHER
-    if comment_re.match(line):
-        line_type = LineType.COMMENT
-    elif domain_re.match(line):
-        result = domain_re.findall(line)[0]
-        if ip_re.match(line):
-            line_type = LineType.IP
-        else:
-            line_type = LineType.DOMAIN
-    else:
-        pass
-    log_line(line, line_type)
-    return result, line_type
-
-
-def process_gfwlist(gfw_list):
-    domain_set = set()
-    ip_set = set()
-    actions = {
-        LineType.DOMAIN: lambda x: domain_set.add(x),
-        LineType.IP: lambda x: ip_set.add(x)
-    }
-
-    for item in gfw_list:
-        result, line_type = parse_line(item)
-        if result:
-            actions.get(line_type)(result)
-
-    return domain_set, ip_set
+def main():
+    context = Context()
+    source = GFWListInput("../gfwlist/gfwlist.txt")
+    source.input(context)
+    target = ClashProviderRuleYml("../gfwlist.yml")
+    target.output(context)
+    target2 = DnsmasqIpsetRuleOutput("../dnsmasq.conf", "gfwlist", ("127.0.0.1", 53))
+    target2.output(context)
 
 
 if __name__ == '__main__':
-    gfwlist = fetch_gfwlist()
-    domain_set, ip_set = process_gfwlist(gfwlist.splitlines())
-    out = dict(payload=["DOMAIN-SUFFIX," + x for x in domain_set])
-    with open("../gfwlist.yml", 'w') as file:
-        file.write(yaml.dump(out))
+    main()
